0c49a0f1b1fe80ac5c80baaf6eb9a279d03c5095
Andrea Guido <57993495+andreaguido@users.noreply.github.com>
2025-05-28 14:21:34 +0200

    creation of otree 5 codes

---
+++ otree5/OnlineExperiment_Asymmetric/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\a.guido\documents\github\labormarketexperiment\onlineexperiment_asymmetric\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
